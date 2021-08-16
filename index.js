0e03dec918aa2c59ad36e2e706d3d4dfb0cb9364
saltyposaune <ewalkertbone@gmail.com>
2021-08-15 18:21:50 -0600

    fixed roles and added styling

---
+++ index.js
@@ -0,0 +1,209 @@
+const generateHTML = require('./src/generateHTML');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const fs = require('fs')
+const inquirer = require('inquirer');
+
+const teamArray = [];
+
+const addManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Manager name:',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else { 
+                console.log("Please enter the manager's name");
+                return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Manager ID:',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else { 
+                    console.log("Please enter the manager's ID number");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Manager email:',
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)  // https://gist.github.com/Amitabh-K/ae073eea3d5207efaddffde19b1618e8
+                if (valid) {
+                    return true;
+                } else {
+                    console.log("Please enter the manager's email address");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Manager office phone number:',
+            validate: officeNumberInput => {
+                if (officeNumberInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the manager's office phone number");
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(managerInput => {
+        const { name, id, email, officeNumber } = managerInput;
+        const manager = new Manager(name, id, email, officeNumber);
+        teamArray.push(manager);
+    });
+};
+
+const addEmployee = () => {
+
+    console.log(`
+    =============================
+    Now add the rest of the team
+    =============================
+    `);
+
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Employee role:',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Employee Name:',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else { 
+                console.log("Please enter the employee's name");
+                return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Employee ID:',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else { 
+                    console.log("Please enter the employee's ID number");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Employee email:',
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log("Please enter the manager's email address");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Employee GitHub username:',
+            when: (input) => input.role ==='Engineer', // https://stackoverflow.com/questions/56412516/conditional-prompt-rendering-in-inquirer
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else { 
+                console.log("Please enter the employee's GitHub username");
+                return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Employee School:',
+            when: (input) => input.role === 'Intern',
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the employee's school");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'additionalEmployee',
+            message: 'Would you like to add another team member?',
+            default: false
+        }
+    ])
+    .then(employeeData => {
+        let { name, id, email, role, github, school, additionalEmployee } = employeeData;
+        let employee;
+        if (role === 'Engineer') {
+            employee = new Engineer(name, id, email, github);
+        } else if (role === 'Intern') {
+            employee = new Intern(name, id, email, school);
+        }
+
+        teamArray.push(employee);
+
+        if (additionalEmployee) {
+            return addEmployee(teamArray);
+        } else {
+            return teamArray;
+        }
+    })
+};
+
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Your team profile has successfully generated");
+        }
+    })
+};
+
+addManager()
+    .then(addEmployee)
+    .then(teamArray => {
+        return generateHTML(teamArray)
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+    
+
+
+        
+        
+    
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,28 +1,28 @@
-const generateManger = function(manager) {
+const generateManager = function (manager) {
     return `
-    <div class="col-4 mt-4>
-        <div class="card>
+    <div class="col-4 mt-4">
+        <div class="card">
             <div class="card-header">
                 <h3>${manager.name}</h3>
-                <h4>Manager</h4>
+                <h4><i class="fas fa-mug-hot"></i> Manager</h4>
             </div>
             <div class="card-body">
                 <p class="id">Employee ID: ${manager.id}</p>
                 <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-                <p class="office-number">ID: ${manager.officeNumber}</p>
+                <p class="office-number">Office Phone: ${manager.officeNumber}</p>
             </div>
         </div>
     </div>
-    ` 
+    `;
 }
 
-const generateEngineer = function(engineer) {
+const generateEngineer = function (engineer) {
     return `
-    <div class="col-4 mt-4>
-        <div class="card>
+    <div class="col-4 mt-4">
+        <div class="card">
             <div class="card-header">
                 <h3>${engineer.name}</h3>
-                <h4>Engineer</h4>
+                <h4><i class="fas fa-desktop"></i> Engineer</h4>
             </div>
             <div class="card-body">
                 <p class="id">Employee ID: ${engineer.id}</p>
@@ -31,16 +31,16 @@ const generateEngineer = function(engineer) {
             </div>
         </div>
     </div>
-    ` 
+    `;
 }
 
-const generateIntern = function(intern) {
+const generateIntern = function (intern) {
     return `
-    <div class="col-4 mt-4>
-        <div class="card>
+    <div class="col-4 mt-4">
+        <div class="card">
             <div class="card-header">
                 <h3>${intern.name}</h3>
-                <h4>Intern</h4>
+                <h4><i class="fas fa-graduation-cap"></i> Intern</h4>
             </div>
             <div class="card-body">
                 <p class="id">Employee ID: ${intern.id}</p>
@@ -49,40 +49,73 @@ const generateIntern = function(intern) {
             </div>
         </div>
     </div>
-    ` 
-}
+    `;
+};
 
 generateHTML = (data) => {
-    pageArray = [];
+
+    pageArray = []; 
 
     for (let i = 0; i < data.length; i++) {
         const employee = data[i];
-        const role = employee.getRole();
+        const role = employee.getRole(); 
 
         if (role === 'Manager') {
             const managerCard = generateManager(employee);
+
             pageArray.push(managerCard);
         }
 
         if (role === 'Engineer') {
             const engineerCard = generateEngineer(employee);
+
             pageArray.push(engineerCard);
         }
-        
+
         if (role === 'Intern') {
             const internCard = generateIntern(employee);
+
             pageArray.push(internCard);
         }
+        
     }
 
     const employeeCards = pageArray.join('');
-    const generateTeam = generatePage(employeeCards);
+    console.log(employeeCards);
+    const generateTeam = generateTeamPage(employeeCards); 
     return generateTeam;
+
 }
 
-const generatePage = function(employeeCards) {
+const generateTeamPage = function (employeeCards) {
     return `
-    
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1 class="title align-middle">Team Profile<h1>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row team-cards" id="team-cards">
+                    ${employeeCards}
+                </div>
+            </div>
+        </main>
+    </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </html>
     `
 }
 
